4ac4500ac1002adb6aa6759898fc60da7d3ddfb9
Jamie Hernandez <litoxl@hotmail.com>
2020-02-11 02:06:34 -0800

    Excelsior!!!

--- src/TestArea.java
+++ src/TestArea.java
@@ -3,6 +3,24 @@ public class TestArea {
 
         int[] setA = new int[0];
         int[] setB = new int[0];
+        ArraySetWithArray<String> set4 = new ArraySetWithArray<>();
+        ArraySetWithArray<String> set5 = new ArraySetWithArray<>();
+        set4.add("C");
+        set4.add("A");
+        set4.add("A");
+        set4.add("A");
+        set4.add("X");
+
+        set5.add("A");
+        set5.add("A");
+        set5.add("A");
+        set5.add("C");
+        set5.add("X");
+
+        set4.moveFirstToEnd();
+        set4.displaySet();
+        set4.moveFirstToEnd();
+        set4.displaySet();
 
     }
 
